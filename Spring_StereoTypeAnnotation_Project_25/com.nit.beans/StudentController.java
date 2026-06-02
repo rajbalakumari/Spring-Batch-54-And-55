d102f4a15c01604112bb82f966e972982e6bc977
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 00:30:11 +0530

    Create StudentController.java

---
+++ Spring_StereoTypeAnnotation_Project_25/com.nit.beans/StudentController.java
@@ -0,0 +1,11 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+      public void getControler()
+      {
+    	  System.out.println("This is a Controller Class.");
+      }
+}
