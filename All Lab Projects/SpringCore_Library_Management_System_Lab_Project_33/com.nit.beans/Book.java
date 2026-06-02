0fd5b5062029c9e99d6cc6f69830fc3025710429
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 01:16:12 +0530

    Create Book.java

---
+++ All Lab Projects/SpringCore_Library_Management_System_Lab_Project_33/com.nit.beans/Book.java
@@ -0,0 +1,47 @@
+package com.nit.beans;
+
+public class Book {
+
+    private int bookId;
+    private String bookName;
+    private String author;
+
+    
+
+    public Book(int bookId, String bookName, String author) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [bookId=" + bookId +
+                ", bookName=" + bookName +
+                ", author=" + author + "]";
+    }
+}
