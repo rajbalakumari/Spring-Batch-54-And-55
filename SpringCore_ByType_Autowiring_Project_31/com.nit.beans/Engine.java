147495228c7ae0a5866476f56f364650ee8c4737
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 01:06:32 +0530

    Create Engine.java

---
+++ SpringCore_ByType_Autowiring_Project_31/com.nit.beans/Engine.java
@@ -0,0 +1,10 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Engine {
+      public void start() {
+    	 System.out.println("Engine has been Started.."); 
+      }
+}
