fe25427f9dd1257b545aeba29c55bb18374319af
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 00:21:05 +0530

    Create Restaurant.java

---
+++ All Lab Projects/Food_Delivery_Management_System_Lab_Project_23/com.nit.beans/Restaurant.java
@@ -0,0 +1,11 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Restaurant {
+     
+	public void showRestaurantService() {
+		System.out.println("Restaurant is preparing fresh food orders for customers.");
+	}
+}
