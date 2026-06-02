e3ab8d50139c55484779c21c78fcad7947eca005
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-02 23:36:28 +0530

    Create AppConfig.java

---
+++ SpringCore_Employee_Management_System_Lab_Project_19/com.nit.config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.nit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+@ComponentScan(basePackages = "com.nit.beans")
+
+public class AppConfig
+{
+
+}
