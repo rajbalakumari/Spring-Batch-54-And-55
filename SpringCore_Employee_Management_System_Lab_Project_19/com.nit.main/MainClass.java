7d03582b811a2db756bb0eb0c417289f2ce72f0b
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-02 23:36:56 +0530

    Create MainClass.java

---
+++ SpringCore_Employee_Management_System_Lab_Project_19/com.nit.main/MainClass.java
@@ -0,0 +1,21 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nit.beans.Company;
+import com.nit.config.AppConfig;
+
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        ApplicationContext context =
+        new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Company company =
+        context.getBean(Company.class);
+
+        company.companyDetails();
+    }
+}
