cfc94f99c419546a3f58147d5a1f3321b21d4910
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-02 21:44:09 +0530

    Create Test.java

---
+++ SpringCore_Project_9/com.nit.main/Test.java
@@ -0,0 +1,19 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nit.beans.Employee;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Employee emp = (Employee) context.getBean("emp");
+
+        emp.display();
+    }
+}
