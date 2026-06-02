27754a1b550df252ca97c08a5f44be98fa87423b
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-02 21:43:16 +0530

    Create Department.java

---
+++ SpringCore_Project_9/com.nit.beans/Department.java
@@ -0,0 +1,25 @@
+package com.nit.beans;
+
+public class Department {
+
+    private int deptId;
+    private String deptName;
+
+    // setters
+    public void setDeptId(int deptId) {
+        this.deptId = deptId;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    // getters
+    public int getDeptId() {
+        return deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+}
