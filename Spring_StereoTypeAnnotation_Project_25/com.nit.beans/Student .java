fa35224a9fc0e402e4af66a89fb0897e61945808
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 00:29:43 +0530

    Create Student .java

---
+++ Spring_StereoTypeAnnotation_Project_25/com.nit.beans/Student .java
@@ -0,0 +1,16 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	public void Studying()
+	{
+		System.out.println("Student bean created");
+		System.out.println("Student is studing java");
+	}
+	
+	
+	
+}
