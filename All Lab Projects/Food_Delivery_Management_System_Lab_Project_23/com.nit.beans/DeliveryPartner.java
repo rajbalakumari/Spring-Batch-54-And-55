837074ccd216471cb1107b830de54b848f290e75
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 00:20:41 +0530

    Create DeliveryPartner.java

---
+++ All Lab Projects/Food_Delivery_Management_System_Lab_Project_23/com.nit.beans/DeliveryPartner.java
@@ -0,0 +1,10 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeliveryPartner {
+     public void showDeliveryService() {
+    	 System.out.println("Delivery partner is delivering orders to customer locations.");
+     }
+}
