f2a12217eb9ca1468c826a4bc13f335ddf23041f
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-02 21:48:26 +0530

    Create DatePrinter.java

---
+++ SpringCore_CurrentDate_Setter_Project_10/com.nit.beans/DatePrinter.java
@@ -0,0 +1,16 @@
+package com.nit.beans;
+
+import java.time.LocalDate;
+
+public class DatePrinter {
+	private LocalDate currentDate;
+
+	public void setCurrentDate(LocalDate currentDate) {
+		this.currentDate = currentDate;
+	}
+
+	public void printDate() {
+		System.out.println("Current Date is: " + currentDate);
+	}
+
+}
