a5ff58415f2ad5a4cf45f58651271890b070bb62
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 00:20:19 +0530

    Create Customer.java

---
+++ All Lab Projects/Food_Delivery_Management_System_Lab_Project_23/com.nit.beans/Customer.java
@@ -0,0 +1,10 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer {
+    public void showCustomerService() {
+    	System.out.println("Customer is placing food orders using mobile application.");
+    }
+}
