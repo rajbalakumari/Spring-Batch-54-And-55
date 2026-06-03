cbf23e98670011e769246064283539e632b86c77
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 12:03:55 +0530

    Create DieselEngine.java

---
+++ All Lab Projects/VehicleEngine_SelectionSystem_Lab_Project_38/com.nit.beans/DieselEngine.java
@@ -0,0 +1,12 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DieselEngine implements Engine {
+
+    @Override
+    public void start() {
+        System.out.println("Diesel engine started.....🚛");
+    }
+}
