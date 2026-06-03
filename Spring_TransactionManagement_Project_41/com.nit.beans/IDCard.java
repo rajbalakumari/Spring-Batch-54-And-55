46bba61a3acb95cd1d00741a2a579643085b618d
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 21:38:32 +0530

    Create IDCard.java

---
+++ Spring_TransactionManagement_Project_41/com.nit.beans/IDCard.java
@@ -0,0 +1,11 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IDCard {
+
+    public void saveCard() {
+        System.out.println("ID Card Details Saved");
+    }
+}
