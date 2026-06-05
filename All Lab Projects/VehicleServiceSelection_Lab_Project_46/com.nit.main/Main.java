b852bc0ab6dde4f8a8925a4c1af5146567a22924
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-05 11:57:20 +0530

    Create Main.java

---
+++ All Lab Projects/VehicleServiceSelection_Lab_Project_46/com.nit.main/Main.java
@@ -0,0 +1,21 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nit.beans.ServiceCenter;
+import com.nit.config.AppConfig;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		ServiceCenter sc = context.getBean(ServiceCenter.class);
+
+		sc.serviceVehicle();
+
+		System.out.println("Application executed successfully");
+	}
+}
