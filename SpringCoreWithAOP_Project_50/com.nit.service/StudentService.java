b9654ec7f59b7d2b904efe9e4310cf401d79c97e
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-08 22:51:35 +0530

    Create StudentService.java

---
+++ SpringCoreWithAOP_Project_50/com.nit.service/StudentService.java
@@ -0,0 +1,12 @@
+package com.nit.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentService {
+
+	public void registerStudent() {
+
+		System.out.println("Student Registered Successfully");
+	}
+}
