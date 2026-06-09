9b13f97104e1a53493df3cdb359622a1efe22917
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-09 15:22:25 +0530

    Create Employee.java

---
+++ All Lab Projects/Spring_TM_EmployeeSalaryRecord_Lab_Project_51/com.nit.beans/Employee.java
@@ -0,0 +1,55 @@
+package com.nit.beans;
+
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+	private String department;
+	private double salary;
+
+	public Employee(int employeeId, String employeeName, String department, double salary) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.department = department;
+		this.salary = salary;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", department=" + department
+				+ ", salary=" + salary + "]";
+	}
+
+}
