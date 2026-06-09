b81e058495fa76fae677a19390323ca834fbb703
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-09 15:24:02 +0530

    Create AppConfig.java

---
+++ All Lab Projects/Spring_TM_EmployeeSalaryRecord_Lab_Project_51/com.nit.config/AppConfig.java
@@ -0,0 +1,37 @@
+package com.nit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "com.nit")
+@EnableTransactionManagement
+public class AppConfig {
+
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		
+		DriverManagerDataSource dm = new DriverManagerDataSource();
+		dm.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dm.setUrl("jdbc:mysql://localhost:3306/spring_jdbc_lab");
+		dm.setUsername("root");
+		dm.setPassword("Root");(change here password)
+		return dm;
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+}
