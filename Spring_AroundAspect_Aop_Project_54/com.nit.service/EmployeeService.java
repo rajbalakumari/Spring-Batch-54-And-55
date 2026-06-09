4e0c7deefbb4d6f1d92cc3f352f170195cd710d6
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-09 23:19:51 +0530

    Create EmployeeService.java

---
+++ Spring_AroundAspect_Aop_Project_54/com.nit.service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.nit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+     public void updateEmployee()
+     {
+    	 System.out.println("Employee Has Been updated..");
+     }
+}
