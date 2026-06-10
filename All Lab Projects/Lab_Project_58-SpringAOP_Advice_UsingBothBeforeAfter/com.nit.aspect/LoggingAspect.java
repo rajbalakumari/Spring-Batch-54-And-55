8f1a0c02af17867d983e1aad975900003150052c
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-10 15:45:04 +0530

    Create LoggingAspect.java

---
+++ All Lab Projects/Lab_Project_58-SpringAOP_Advice_UsingBothBeforeAfter/com.nit.aspect/LoggingAspect.java
@@ -0,0 +1,23 @@
+package com.nit.aspect;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.nit.aspect.StudentService.*(..))")
+    public void beforeAdvice() {
+
+        System.out.println("Method Started");
+    }
+
+    @After("execution(* com.nit.aspect.StudentService.*(..))")
+    public void afterAdvice() {
+
+        System.out.println("Method Completed");
+    }
+}
