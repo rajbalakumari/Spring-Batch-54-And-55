c5125598301ddf0e06e7a95d52a5e5cbfff3e6ff
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-10 22:58:52 +0530

    Create CreditCardPayment.java

---
+++ Project_63-StrategyDesignPattern/com.nit/CreditCardPayment.java
@@ -0,0 +1,9 @@
+package com.nit;
+
+public class CreditCardPayment implements PaymentStrategy {
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid Rs." + amount + " using Credit Card");
+    }
+}
