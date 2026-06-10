8f1e5ce067f4878ff22981e3288606e206cfe525
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-10 22:53:12 +0530

    Create BankService.java

---
+++ Project_61-TransactionManagementWithAop/com.nit.service/BankService.java
@@ -0,0 +1,10 @@
+package com.nit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankService {
+       public void transferMoney() {
+    	   System.out.println("Money Transer");
+       }
+}
