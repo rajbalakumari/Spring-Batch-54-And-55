7a50ea071825aa3417fad7d281007b634b3d9889
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-10 15:34:46 +0530

    Create Main.java

---
+++ All Lab Projects/Lab_Project_55-SpringAOP_Print_LoggingMessage/com.nit.main/Main.java
@@ -0,0 +1,16 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nit.aspect.StudentService;
+import com.nit.config.AppConfig;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+		StudentService service = context.getBean(StudentService.class);
+		service.saveStudent();
+		
+	}
+}
