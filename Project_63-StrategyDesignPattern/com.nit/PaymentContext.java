8ab316ba05f3033fccb9b41010207b92c55c6baa
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-10 22:59:29 +0530

    Create PaymentContext.java

---
+++ Project_63-StrategyDesignPattern/com.nit/PaymentContext.java
@@ -0,0 +1,14 @@
+package com.nit;
+
+public class PaymentContext {
+
+    private PaymentStrategy paymentStrategy;
+
+    public PaymentContext(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void makePayment(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
