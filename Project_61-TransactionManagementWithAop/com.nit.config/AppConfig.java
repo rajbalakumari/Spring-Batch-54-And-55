ba5679dd543290dae68a1c65198da40988d2d332
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-10 22:52:48 +0530

    Create AppConfig.java

---
+++ Project_61-TransactionManagementWithAop/com.nit.config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.nit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages  = "com.nit")
+@EnableAspectJAutoProxy
+public class AppConfig {
+
+}
