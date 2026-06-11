a533da913b07a6830893dffcf705aba63fb75ade
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-11 22:28:58 +0530

    Create ExcelReport.java

---
+++ All Lab Projects/Lab_Project_64-ReportGenerationFactoryDesignPattern/com.nit/ExcelReport.java
@@ -0,0 +1,9 @@
+package com.nit;
+
+public class ExcelReport implements Report {
+
+    @Override
+    public void generateReport() {
+        System.out.println("Generating Excel report.");
+    }
+}
