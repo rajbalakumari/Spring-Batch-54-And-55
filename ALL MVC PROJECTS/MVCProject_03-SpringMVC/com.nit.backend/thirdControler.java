68c0811d6b0e4ca95fd74ffa3c8e76febc980032
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-18 21:49:46 +0530

    Create thirdControler.java

---
+++ ALL MVC PROJECTS/MVCProject_03-SpringMVC/com.nit.backend/thirdControler.java
@@ -0,0 +1,13 @@
+package com.nit.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class thirdControler {
+    
+	@RequestMapping("/home")
+	public String getHomePage() {
+		return "homePage";
+	}
+}
