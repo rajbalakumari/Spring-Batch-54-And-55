c58cd5a8c1771cb13be69abc1e0629afd21512aa
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-24 22:46:17 +0530

    Create Student.java

---
+++ ALL MVC PROJECTS/MVCProject_06-StudentRegistrationForm/com.nit.controler/Student.java
@@ -0,0 +1,35 @@
+package com.nit.controler;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String course;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
