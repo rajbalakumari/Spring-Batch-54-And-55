9219f801ddd5cc88db433f26d47dde76556f5ffe
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-25 22:50:50 +0530

    Create Feedback.java

---
+++ ALL MVC PROJECTS/ALL lab Projects On MVC/MVC_Lab_Projects_05-AllTraditional_Assignments/com.nit.backend/Feedback.java
@@ -0,0 +1,50 @@
+package com.nit.backend;
+
+public class Feedback {
+
+    private String customerName;
+    private String mobileNumber;
+    private String productName;
+    private int rating;
+    private String feedback;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
