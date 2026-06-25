e392e78a3d0caec7612f92331bf4fd294483ba0d
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-25 22:52:02 +0530

    Create Student.java

---
+++ ALL MVC PROJECTS/ALL lab Projects On MVC/MVC_Lab_Projects_05-AllTraditional_Assignments/com.nit.backend/Student.java
@@ -0,0 +1,41 @@
+package com.nit.backend;
+
+public class Student {
+
+    private int studentId;
+    private String studentName;
+    private String course;
+    private String email;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
