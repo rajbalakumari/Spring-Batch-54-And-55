e0d45323552a3e1b4bc5e5d6a6c3eba3e9075e52
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-25 22:49:16 +0530

    Create Admission.java

---
+++ ALL MVC PROJECTS/ALL lab Projects On MVC/MVC_Lab_Projects_05-AllTraditional_Assignments/com.nit.backend/Admission.java
@@ -0,0 +1,59 @@
+package com.nit.backend;
+
+public class Admission {
+
+    private String studentName;
+    private String fatherName;
+    private String email;
+    private String mobileNumber;
+    private String course;
+    private String city;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
