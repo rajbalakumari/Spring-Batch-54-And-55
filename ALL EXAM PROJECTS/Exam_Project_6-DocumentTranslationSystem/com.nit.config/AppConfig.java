dbe31cb762762583b34271393b672886691bc96a
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-04 22:35:43 +0530

    Create AppConfig.java

---
+++ ALL EXAM PROJECTS/Exam_Project_6-DocumentTranslationSystem/com.nit.config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.nit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@ComponentScan(basePackages = "com.nit")
+@Configuration
+public class AppConfig {
+    
+}
