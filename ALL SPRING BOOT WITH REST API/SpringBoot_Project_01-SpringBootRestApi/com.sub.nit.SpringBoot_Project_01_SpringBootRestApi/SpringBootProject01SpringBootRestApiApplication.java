ca67e573a311ca4e8e0047245e844c77e4607fc3
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-10 23:20:45 +0530

    Create SpringBootProject01SpringBootRestApiApplication.java

---
+++ ALL SPRING BOOT WITH REST API/SpringBoot_Project_01-SpringBootRestApi/com.sub.nit.SpringBoot_Project_01_SpringBootRestApi/SpringBootProject01SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.SpringBoot_Project_01_SpringBootRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject01SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject01SpringBootRestApiApplication.class, args);
+	}
+
+}
