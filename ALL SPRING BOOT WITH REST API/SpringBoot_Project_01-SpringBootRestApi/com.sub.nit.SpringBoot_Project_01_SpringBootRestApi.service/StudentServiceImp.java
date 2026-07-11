04e329515d65b7ba7a85d58528ddf7e05049c82d
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-11 21:55:57 +0530

    Update StudentServiceImp.java

--- ALL SPRING BOOT WITH REST API/SpringBoot_Project_01-SpringBootRestApi/com.sub.nit.SpringBoot_Project_01_SpringBootRestApi.service/StudentServiceImp.java
+++ ALL SPRING BOOT WITH REST API/SpringBoot_Project_01-SpringBootRestApi/com.sub.nit.SpringBoot_Project_01_SpringBootRestApi.service/StudentServiceImp.java
@@ -1,5 +1,50 @@
 package com.sub.nit.SpringBoot_Project_01_SpringBootRestApi.service;
 
-public class StudentServiceImp {
-      
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sub.nit.SpringBoot_Project_01_SpringBootRestApi.entity.Student;
+import com.sub.nit.SpringBoot_Project_01_SpringBootRestApi.repository.StudentRepository;
+
+@Service
+public class StudentServiceImp implements StudentService {
+
+    @Autowired
+    private StudentRepository repo;
+
+    @Override
+    public List<Student> getAllStudent() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Student getAllStudentById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public void saveStudent(Student s) {
+        repo.save(s);
+    }
+
+    @Override
+    public void updateStudent(int id, Student s) {
+
+        Student student = repo.findById(id).orElse(null);
+
+        if (student != null) {
+            student.setName(s.getName());
+            student.setBranch(s.getBranch());
+            student.setMobNum(s.getMobNum());
+
+            repo.save(student);
+        }
+    }
+
+    @Override
+    public void deleteStudent(int id) {
+        repo.deleteById(id);
+    }
 }
