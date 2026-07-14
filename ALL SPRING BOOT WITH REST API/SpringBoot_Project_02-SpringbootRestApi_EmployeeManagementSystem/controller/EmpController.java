950c52e9245658634df68896736bd33e1b4e846a
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-14 22:35:17 +0530

    Update EmpController.java

--- ALL SPRING BOOT WITH REST API/SpringBoot_Project_02-SpringbootRestApi_EmployeeManagementSystem/controller/EmpController.java
+++ ALL SPRING BOOT WITH REST API/SpringBoot_Project_02-SpringbootRestApi_EmployeeManagementSystem/controller/EmpController.java
@@ -3,23 +3,48 @@ package com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem.controller
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem.entity.Employee;
+import com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem.enitity.Employee;
 import com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem.service.EmployeeService;
 
 @RestController
 public class EmpController {
-	
+
 	@Autowired
 	EmployeeService service;
+
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return service.getAllEmployees();
 	}
-	@GetMapping("/employee")
-	public Employee getEmployeeById(int id) {
-		return service.getEmployeeById(id);
+
+	@GetMapping("/employee/{id}")
+	public Employee getEmployeeBasedOnId(@PathVariable int id) {
+		Employee emp = service.getEmployeeById(id);
+		return emp;
 	}
+	@PostMapping("/employee/add")
+	public void createEmployee(@RequestBody Employee emp) {
+		service.saveEmployee(emp);
+	}
+	
+	@PutMapping("/employee/update/{id}")
+	public Employee updateEmployee(@PathVariable int id ,@RequestBody Employee emp) {
+		return service.updateEmployee(id, emp);
+	}
+	
+	
+	@DeleteMapping("/employee/delete/{id}")
+	public String deleteEmployee(@PathVariable int id ) {
+		service.deleteEmployee(id);
+		return "Deleted";
+	}
+	
 }
