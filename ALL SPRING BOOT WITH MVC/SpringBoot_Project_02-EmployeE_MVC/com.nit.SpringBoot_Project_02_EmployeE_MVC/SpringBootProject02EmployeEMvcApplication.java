342d37ba51d7db6ab6227b7ed85df4494d56f40c
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-15 21:42:54 +0530

    Create SpringBootProject02EmployeEMvcApplication.java

---
+++ ALL SPRING BOOT WITH MVC/SpringBoot_Project_02-EmployeE_MVC/com.nit.SpringBoot_Project_02_EmployeE_MVC/SpringBootProject02EmployeEMvcApplication.java
@@ -0,0 +1,13 @@
+package com.nit.SpringBoot_Project_02_EmployeE_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject02EmployeEMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject02EmployeEMvcApplication.class, args);
+	}
+
+}
