81dc20dcb52e1a4a7cd6c0a76152e10d86d1f76c
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-18 22:23:43 +0530

    Create SpringBootProject03BootWithDataJpaApplication.java

---
+++ ALL SPRING BOOT WITH DATA JPA/SpringBoot_Project_03-BootWithDataJPA/com.sub.nit.jpa/SpringBootProject03BootWithDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject03BootWithDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject03BootWithDataJpaApplication.class, args);
+	}
+
+}
