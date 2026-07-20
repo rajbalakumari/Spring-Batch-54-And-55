fbc0830f9280e562f2ac5a310b74bd60b5a8eacc
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-20 21:54:28 +0530

    Create Product.java

---
+++ ALL SPRING BOOT WITH DATA JPA/SpringBoot_Project_04-RestApi_DataJPA/com.sub.nit.jpa.entity/Product.java
@@ -0,0 +1,90 @@
+package com.sub.nit.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id")
+	private int id;
+
+	@Column(name = "product_name")
+	private String name;
+
+	@Column(name = "product_brand")
+	private String brand;
+
+	@Column(name = "product_category")
+	private String category;
+
+	@Column(name = "product_price")
+	private double price;
+
+	@Column(name = "product_quantity")
+	private int quantity;
+
+	public Product() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", brand=" + brand + ", category=" + category + ", price="
+				+ price + ", quantity=" + quantity + "]";
+	}
+}
