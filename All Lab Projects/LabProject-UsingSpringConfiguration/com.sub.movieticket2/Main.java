85d8c8eefa08b825fae4f7070702c5a29c75a30c
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-23 22:34:00 +0530

    Create Main.java

---
+++ All Lab Projects/LabProject-UsingSpringConfiguration/com.sub.movieticket2/Main.java
@@ -0,0 +1,15 @@
+package com.sub.movieticket2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		MovieTicket movieTicket = context.getBean(MovieTicket.class);
+		movieTicket.bookTicket();
+	}
+}
