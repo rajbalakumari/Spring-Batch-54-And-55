31144b9ce1e394a0d88623e0d74eff9d3c3f2980
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-23 22:32:57 +0530

    Create DoctorService.java

---
+++ All Lab Projects/LabProject-UsingSpringConfiguration/com.sub.hospital3/DoctorService.java
@@ -0,0 +1,10 @@
+package com.sub.hospital3;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DoctorService {
+
+    public void consult() {
+        System.out.println("Doctor Consultation Completed");
+    }
+}
