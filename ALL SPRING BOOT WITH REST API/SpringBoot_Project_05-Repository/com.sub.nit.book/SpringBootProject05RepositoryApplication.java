776886721009217583f10d080cd9fdb83fe35a64
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-24 22:26:20 +0530

    Create SpringBootProject05RepositoryApplication.java

---
+++ ALL SPRING BOOT WITH REST API/SpringBoot_Project_05-Repository/com.sub.nit.book/SpringBootProject05RepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject05RepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject05RepositoryApplication.class, args);
+	}
+
+}
