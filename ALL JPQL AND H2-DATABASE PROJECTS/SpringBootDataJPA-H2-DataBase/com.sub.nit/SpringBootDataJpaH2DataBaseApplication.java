24b03ccd056ff5a98ed14d7478be3f62eef459ad
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-27 22:14:49 +0530

    Create SpringBootDataJpaH2DataBaseApplication.java

---
+++ ALL JPQL AND H2-DATABASE PROJECTS/SpringBootDataJPA-H2-DataBase/com.sub.nit/SpringBootDataJpaH2DataBaseApplication.java
@@ -0,0 +1,15 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaH2DataBaseApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootDataJpaH2DataBaseApplication.class, args);
+
+    }
+
+}
