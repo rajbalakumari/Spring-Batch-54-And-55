6ad4aa5ee98d5a64d119f3f1734e21f498464db8
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-27 22:10:41 +0530

    Create JpqlProjectt01SpringBootApplication.java

---
+++ ALL JPQL AND H2-DATABASE PROJECTS/JPQL-Projectt01-SpringBoot/com.sub.nit/JpqlProjectt01SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpqlProjectt01SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpqlProjectt01SpringBootApplication.class, args);
+	}
+
+}
