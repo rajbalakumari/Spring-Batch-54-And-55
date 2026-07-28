a180f88eedb2af1d03758cc2cb0801395faee801
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-28 21:17:50 +0530

    Create SpringBootProject06OneToOneApplication.java

---
+++ ALL MAPING PROJECTS/SpringBoot_Project_06-One-To-One/com.sub.nit/SpringBootProject06OneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject06OneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject06OneToOneApplication.class, args);
+	}
+
+}
