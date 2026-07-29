e9d13b8eb10543075ae84cf9b73f3bd1f7a17244
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-29 22:25:03 +0530

    Create SpringBootProject08ManyToOneApplication.java

---
+++ ALL MAPING PROJECTS/SpringBoot_Project_08-Many-To-One/com.sub.nit/SpringBootProject08ManyToOneApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject08ManyToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject08ManyToOneApplication.class, args);
+	}
+
+}
