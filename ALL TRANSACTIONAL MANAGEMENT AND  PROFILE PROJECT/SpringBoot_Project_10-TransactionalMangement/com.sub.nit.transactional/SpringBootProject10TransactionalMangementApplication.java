dfb158ffeb0810ad29dc1bbdeac6130c7d7e679f
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-29 22:45:01 +0530

    Create SpringBootProject10TransactionalMangementApplication.java

---
+++ ALL TRANSACTIONAL MANAGEMENT AND  PROFILE PROJECT/SpringBoot_Project_10-TransactionalMangement/com.sub.nit.transactional/SpringBootProject10TransactionalMangementApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject10TransactionalMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject10TransactionalMangementApplication.class, args);
+	}
+
+}
