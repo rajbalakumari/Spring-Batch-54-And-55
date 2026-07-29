ffa19cf87b85cd76f046612ef8df5847473259d1
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-29 22:29:23 +0530

    Create SpringBootProject09ManyToManyApplication.java

---
+++ ALL MAPING PROJECTS/SpringBoot_Project_09-Many-To-Many/com.sub.nit/SpringBootProject09ManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject09ManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject09ManyToManyApplication.class, args);
+	}
+
+}
