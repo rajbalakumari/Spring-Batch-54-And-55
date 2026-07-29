786bc9d109750508d886c246c0ec08bf61aefa98
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-29 22:29:49 +0530

    Create StudentController.java

---
+++ ALL MAPING PROJECTS/SpringBoot_Project_09-Many-To-Many/com.sub.nit.controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.sub.nit.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.sub.nit.entity.Student;
+import com.sub.nit.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @PostMapping("/save")
+    public Student save(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+
+    @GetMapping("/all")
+    public List<Student> getAllStudents() {
+        return service.getAllStudents();
+    }
+}
