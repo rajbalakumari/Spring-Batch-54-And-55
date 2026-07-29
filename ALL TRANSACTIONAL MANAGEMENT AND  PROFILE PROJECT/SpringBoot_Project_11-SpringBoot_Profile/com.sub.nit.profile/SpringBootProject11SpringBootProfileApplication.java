97495277d8d8aff1402c6044742a90af314a28a0
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-29 22:48:59 +0530

    Create SpringBootProject11SpringBootProfileApplication.java

---
+++ ALL TRANSACTIONAL MANAGEMENT AND  PROFILE PROJECT/SpringBoot_Project_11-SpringBoot_Profile/com.sub.nit.profile/SpringBootProject11SpringBootProfileApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject11SpringBootProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject11SpringBootProfileApplication.class, args);
+	}
+
+}
