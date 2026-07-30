cdbcd0b125dbdb0cf68d67e20dd513779f305114
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-30 21:48:22 +0530

    Create SpringBootProject12ValidationApplication.java

---
+++ ALL SPRING BOOT PROJECTS/SpringBoot_Project_12-Validation/com.sub.nit/SpringBootProject12ValidationApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject12ValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject12ValidationApplication.class, args);
+	}
+
+}
