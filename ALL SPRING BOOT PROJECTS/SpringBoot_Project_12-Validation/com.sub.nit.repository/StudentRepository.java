066eccd17e0299d24e32b257b22916078f24f099
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-30 21:49:48 +0530

    Create StudentRepository.java

---
+++ ALL SPRING BOOT PROJECTS/SpringBoot_Project_12-Validation/com.sub.nit.repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.sub.nit.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sub.nit.entity.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer>{
+
+}
