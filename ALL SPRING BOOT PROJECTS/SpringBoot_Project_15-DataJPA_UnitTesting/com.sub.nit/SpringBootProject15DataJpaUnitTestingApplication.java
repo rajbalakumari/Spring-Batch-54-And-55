a79de55762f7bdf3a4fba1ec6734c486e97d408b
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-31 22:56:16 +0530

    Create SpringBootProject15DataJpaUnitTestingApplication.java

---
+++ ALL SPRING BOOT PROJECTS/SpringBoot_Project_15-DataJPA_UnitTesting/com.sub.nit/SpringBootProject15DataJpaUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject15DataJpaUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject15DataJpaUnitTestingApplication.class, args);
+	}
+
+}
