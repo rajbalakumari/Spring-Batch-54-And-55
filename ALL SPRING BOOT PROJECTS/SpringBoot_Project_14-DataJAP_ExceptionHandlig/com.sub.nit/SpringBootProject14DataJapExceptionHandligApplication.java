6284463b8890f4d3509a9cf5bb202f3274528279
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-31 22:42:00 +0530

    Create SpringBootProject14DataJapExceptionHandligApplication.java

---
+++ ALL SPRING BOOT PROJECTS/SpringBoot_Project_14-DataJAP_ExceptionHandlig/com.sub.nit/SpringBootProject14DataJapExceptionHandligApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject14DataJapExceptionHandligApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject14DataJapExceptionHandligApplication.class, args);
+	}
+
+}
