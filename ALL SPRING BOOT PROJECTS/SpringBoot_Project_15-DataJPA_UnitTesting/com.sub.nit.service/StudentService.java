46f7efe96d0fc380bf845fd13ec6e09106dc75fc
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-31 22:57:56 +0530

    Create StudentService.java

---
+++ ALL SPRING BOOT PROJECTS/SpringBoot_Project_15-DataJPA_UnitTesting/com.sub.nit.service/StudentService.java
@@ -0,0 +1,19 @@
+package com.sub.nit.service;
+
+import java.util.List;
+
+import com.sub.nit.entity.Student;
+
+public interface StudentService {
+
+    Student saveStudent(Student student);
+
+    Student getStudentById(Integer id);
+
+    List<Student> getAllStudents();
+
+    Student updateStudent(Student student);
+
+    void deleteStudent(Integer id);
+
+}
