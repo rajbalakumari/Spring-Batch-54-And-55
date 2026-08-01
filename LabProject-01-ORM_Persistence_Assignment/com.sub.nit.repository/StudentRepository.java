5677108a941312a01cf9cd076eb9d97183769d15
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-01 15:15:15 +0530

    Create StudentRepository.java

---
+++ LabProject-01-ORM_Persistence_Assignment/com.sub.nit.repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.sub.nit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sub.nit.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student,Integer>{
+
+}
