660c9855b22a2d8bf7a30f0b1d3f6765315e9a30
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-01 15:54:41 +0530

    Update Student.java

--- LabProject-01-ORM_Persistence_Assignment/com.sub.nit.entity/Student.java
+++ LabProject-01-ORM_Persistence_Assignment/com.sub.nit.entity/Student.java
@@ -1,12 +1,11 @@
 package com.sub.nit.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Student {
 
     @Id
@@ -18,4 +17,58 @@ public class Student {
     private String course;
 
     private Double fee;
+
+    
+    public Student() {
+    }
+
+    
+    public Student(Integer studentId, String name, String course, Double fee) {
+        this.studentId = studentId;
+        this.name = name;
+        this.course = course;
+        this.fee = fee;
+    }
+
+   
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+e
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+   
+    public Double getFee() {
+        return fee;
+    }
+
+    public void setFee(Double fee) {
+        this.fee = fee;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Student [studentId=" + studentId + ", name=" + name
+                + ", course=" + course + ", fee=" + fee + "]";
+    }
 }
