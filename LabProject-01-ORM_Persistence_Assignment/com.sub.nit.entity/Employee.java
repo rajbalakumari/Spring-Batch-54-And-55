0ade0fd3ed71ccbfbe77db3983778df6badd57bc
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-01 15:53:37 +0530

    Update Employee.java

--- LabProject-01-ORM_Persistence_Assignment/com.sub.nit.entity/Employee.java
+++ LabProject-01-ORM_Persistence_Assignment/com.sub.nit.entity/Employee.java
@@ -4,14 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Employee {
 
     @Id
@@ -19,9 +13,48 @@ public class Employee {
     private Integer employeeId;
 
     private String employeeName;
-
     private String department;
-
     private Double salary;
 
+    public Employee() {
+    }
+
+    public Employee(Integer employeeId, String employeeName, String department, Double salary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }
