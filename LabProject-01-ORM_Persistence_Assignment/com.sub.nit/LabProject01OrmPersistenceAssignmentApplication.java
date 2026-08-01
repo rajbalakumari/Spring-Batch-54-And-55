e200b6b7c307265e39f7d99890c5e91627be3a08
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-01 15:10:40 +0530

    Create LabProject01OrmPersistenceAssignmentApplication.java

---
+++ LabProject-01-ORM_Persistence_Assignment/com.sub.nit/LabProject01OrmPersistenceAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabProject01OrmPersistenceAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabProject01OrmPersistenceAssignmentApplication.class, args);
+	}
+
+}
