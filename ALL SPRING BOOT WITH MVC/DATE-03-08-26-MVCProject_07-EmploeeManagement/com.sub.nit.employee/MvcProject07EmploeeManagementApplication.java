47f12710f9e019d1c7b680630964231c508cf981
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-03 20:36:20 +0530

    Create MvcProject07EmploeeManagementApplication.java

---
+++ ALL SPRING BOOT WITH MVC/DATE-03-08-26-MVCProject_07-EmploeeManagement/com.sub.nit.employee/MvcProject07EmploeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProject07EmploeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProject07EmploeeManagementApplication.class, args);
+	}
+
+}
