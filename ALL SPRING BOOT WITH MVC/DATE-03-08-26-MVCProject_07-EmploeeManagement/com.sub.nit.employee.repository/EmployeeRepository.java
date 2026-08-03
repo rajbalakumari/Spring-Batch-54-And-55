0acc8209c080a6cccd5f7ec13efe1c9c94f867f4
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-03 20:38:38 +0530

    Create EmployeeRepository.java

---
+++ ALL SPRING BOOT WITH MVC/DATE-03-08-26-MVCProject_07-EmploeeManagement/com.sub.nit.employee.repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.sub.nit.employee.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sub.nit.employee.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
