b431a360c4fedefba5ff739b66d54e2d214fb31f
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-04 21:10:32 +0530

    Create SpringBootProject16ProductManagementSystemApplication.java

---
+++ ALL SPRING BOOT WITH REST API/DATE-04-08-26-Responding Annotation And Actuator/SpringBoot_Project_16-ProductManagementSystem/com.sub.nit.product/SpringBootProject16ProductManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject16ProductManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject16ProductManagementSystemApplication.class, args);
+	}
+
+}
