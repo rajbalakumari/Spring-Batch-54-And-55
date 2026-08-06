417b7f3642a2385d76be06ceba14ace33800481b
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-06 22:03:36 +0530

    Create SpringBootProject19SwaggerDocumentationApplication.java

---
+++ ALL SPRING BOOT WITH REST API/DATE-06-08-26-SWAGGER DOCUMENTATION AND EXCEPTION WITH SWAGGER AND RESPONSE/SpringBoot_Project_19-SwaggerDocumentation/com.sub.nit;/SpringBootProject19SwaggerDocumentationApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject19SwaggerDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject19SwaggerDocumentationApplication.class, args);
+	}
+
+}
