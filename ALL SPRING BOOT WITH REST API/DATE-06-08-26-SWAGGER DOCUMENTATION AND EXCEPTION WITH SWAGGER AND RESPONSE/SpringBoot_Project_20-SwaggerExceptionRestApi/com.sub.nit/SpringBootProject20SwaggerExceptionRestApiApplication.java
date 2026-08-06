382275a8e5ddbf87c65de79f2048b2d4ebd6a60e
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-06 22:10:13 +0530

    Create SpringBootProject20SwaggerExceptionRestApiApplication.java

---
+++ ALL SPRING BOOT WITH REST API/DATE-06-08-26-SWAGGER DOCUMENTATION AND EXCEPTION WITH SWAGGER AND RESPONSE/SpringBoot_Project_20-SwaggerExceptionRestApi/com.sub.nit/SpringBootProject20SwaggerExceptionRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject20SwaggerExceptionRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject20SwaggerExceptionRestApiApplication.class, args);
+	}
+
+}
