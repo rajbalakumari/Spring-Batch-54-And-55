ae1958487145018dfe267cb3109d4603f589b54f
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-07 21:35:03 +0530

    Create SpringBootProject21SecurityWithWaggerApplication.java

---
+++ ALL SPRING SECURITY PROJECTS/SpringBoot_Project_21-SecurityWithSWagger/com.nit.security/SpringBootProject21SecurityWithWaggerApplication.java
@@ -0,0 +1,13 @@
+package com.nit.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject21SecurityWithWaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject21SecurityWithWaggerApplication.class, args);
+	}
+
+}
