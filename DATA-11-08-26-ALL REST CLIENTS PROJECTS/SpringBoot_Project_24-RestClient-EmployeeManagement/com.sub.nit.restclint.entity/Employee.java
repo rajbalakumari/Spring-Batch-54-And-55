7944b0d8a1f627ab76447c733708ebfd903a4dd8
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-11 22:08:19 +0530

    Create Employee.java

---
+++ DATA-11-08-26-ALL REST CLIENTS PROJECTS/SpringBoot_Project_24-RestClient-EmployeeManagement/com.sub.nit.restclint.entity/Employee.java
@@ -0,0 +1,60 @@
+package com.sub.nit.restclint.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String email;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, String email, double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
