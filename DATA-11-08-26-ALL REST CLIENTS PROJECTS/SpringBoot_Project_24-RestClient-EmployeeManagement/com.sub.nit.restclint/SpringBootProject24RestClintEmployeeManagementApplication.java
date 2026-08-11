4cca014d21a70ca3fa93cb545ccbd0de871644f5
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-11 22:07:26 +0530

    Create SpringBootProject24RestClintEmployeeManagementApplication.java

---
+++ DATA-11-08-26-ALL REST CLIENTS PROJECTS/SpringBoot_Project_24-RestClient-EmployeeManagement/com.sub.nit.restclint/SpringBootProject24RestClintEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.restclint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject24RestClintEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject24RestClintEmployeeManagementApplication.class, args);
+	}
+
+}
