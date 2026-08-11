fb820c9f748d61ea71462cafbb863cbff9ba7a66
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-11 22:21:39 +0530

    Create SpringBootProject26RestTemplateApplication.java

---
+++ DATA-11-08-26-ALL REST CLIENTS PROJECTS/SpringBoot_Project_26-RestTemplate/com.sub.nit.restclient/SpringBootProject26RestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject26RestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject26RestTemplateApplication.class, args);
+	}
+
+}
