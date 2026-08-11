7b666e9670686f532ed24d0bd8577c19732446a1
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-11 22:16:32 +0530

    Create SpringBootProject25RestClientApisApplication.java

---
+++ DATA-11-08-26-ALL REST CLIENTS PROJECTS/SpringBoot_Project_25-WebClientApis/com.sub.nit.restclient/SpringBootProject25RestClientApisApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject25RestClientApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject25RestClientApisApplication.class, args);
+	}
+
+}
