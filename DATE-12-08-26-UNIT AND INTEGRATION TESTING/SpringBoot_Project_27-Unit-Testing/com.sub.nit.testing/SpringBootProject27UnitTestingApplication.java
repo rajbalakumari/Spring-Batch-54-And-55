9607ecf8f97b729c30890c5fb43700876d34c385
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-12 22:05:45 +0530

    Create SpringBootProject27UnitTestingApplication.java

---
+++ DATE-12-08-26-UNIT AND INTEGRATION TESTING/SpringBoot_Project_27-Unit-Testing/com.sub.nit.testing/SpringBootProject27UnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject27UnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject27UnitTestingApplication.class, args);
+	}
+
+}
