1e9eaac716e1cf26a30f1e3ef536a631a60c28b2
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-12 22:31:17 +0530

    Create SpringBootProject28IntegrationTestingApplication.java

---
+++ DATE-12-08-26-UNIT AND INTEGRATION TESTING/SpringBoot_Project_28-Integration-Testing/com.sub.nit.integration/SpringBootProject28IntegrationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject28IntegrationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject28IntegrationTestingApplication.class, args);
+	}
+
+}
