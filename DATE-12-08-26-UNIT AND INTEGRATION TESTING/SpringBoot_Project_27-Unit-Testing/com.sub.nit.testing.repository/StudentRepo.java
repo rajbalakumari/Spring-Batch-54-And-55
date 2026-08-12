f5e9a1d268da35d4def38b31d4118ac6576aa770
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-12 22:07:08 +0530

    Create StudentRepo.java

---
+++ DATE-12-08-26-UNIT AND INTEGRATION TESTING/SpringBoot_Project_27-Unit-Testing/com.sub.nit.testing.repository/StudentRepo.java
@@ -0,0 +1,11 @@
+package com.sub.nit.testing.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sub.nit.testing.entity.Student;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+}
