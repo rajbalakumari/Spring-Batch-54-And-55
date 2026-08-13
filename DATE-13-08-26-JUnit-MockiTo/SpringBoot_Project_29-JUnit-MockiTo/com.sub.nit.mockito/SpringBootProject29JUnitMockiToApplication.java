e879867efcad940e21c51bb086d8dca393c46f31
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-13 19:43:57 +0530

    Create SpringBootProject29JUnitMockiToApplication.java

---
+++ DATE-13-08-26-JUnit-MockiTo/SpringBoot_Project_29-JUnit-MockiTo/com.sub.nit.mockito/SpringBootProject29JUnitMockiToApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject29JUnitMockiToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject29JUnitMockiToApplication.class, args);
+	}
+
+}
