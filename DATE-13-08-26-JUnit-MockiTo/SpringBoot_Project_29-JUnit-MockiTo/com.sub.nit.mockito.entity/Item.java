4a5be00d42f49b9d585fca99aab2e0f5f9ea805b
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-13 19:44:50 +0530

    Create Item.java

---
+++ DATE-13-08-26-JUnit-MockiTo/SpringBoot_Project_29-JUnit-MockiTo/com.sub.nit.mockito.entity/Item.java
@@ -0,0 +1,56 @@
+package com.sub.nit.mockito.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+
+    public Item() {
+    }
+
+    public Item(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+    
+}
