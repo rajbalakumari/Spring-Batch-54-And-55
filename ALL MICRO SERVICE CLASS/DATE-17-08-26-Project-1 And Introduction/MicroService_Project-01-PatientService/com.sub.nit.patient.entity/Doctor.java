338549ec9e99b51b8632dc0eb753f38f9c0766fb
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-17 19:08:54 +0530

    Create Doctor.java

---
+++ ALL MICRO SERVICE CLASS/DATE-17-08-26-Project-1 And Introduction/MicroService_Project-01-PatientService/com.sub.nit.patient.entity/Doctor.java
@@ -0,0 +1,41 @@
+package com.sub.nit.patient.entity;
+
+public class Doctor {
+
+	private int id;
+	private String name;
+	private String specialization;
+
+	public Doctor() {
+	}
+
+	public Doctor(int id, String name, String specialization) {
+		this.id = id;
+		this.name = name;
+		this.specialization = specialization;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+}
