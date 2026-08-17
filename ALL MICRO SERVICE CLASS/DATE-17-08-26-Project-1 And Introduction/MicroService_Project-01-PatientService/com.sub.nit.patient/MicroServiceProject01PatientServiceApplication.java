86af3d4cb25b25307ccfd4db99f4a636b52dd4ad
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-17 19:07:37 +0530

    Create MicroServiceProject01PatientServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-17-08-26-Project-1 And Introduction/MicroService_Project-01-PatientService/com.sub.nit.patient/MicroServiceProject01PatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject01PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject01PatientServiceApplication.class, args);
+	}
+
+}
