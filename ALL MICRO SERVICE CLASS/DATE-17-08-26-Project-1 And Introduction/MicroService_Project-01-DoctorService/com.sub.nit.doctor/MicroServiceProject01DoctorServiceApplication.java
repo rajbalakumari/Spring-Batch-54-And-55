f8a44b0808a977f04f83abb1b26c1dc53c46eeb9
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-17 19:01:50 +0530

    Create MicroServiceProject01DoctorServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-17-08-26-Project-1 And Introduction/MicroService_Project-01-DoctorService/com.sub.nit.doctor/MicroServiceProject01DoctorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject01DoctorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject01DoctorServiceApplication.class, args);
+	}
+
+}
