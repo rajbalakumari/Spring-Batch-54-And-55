3bd092abb959506ba12e1706e14b305ff02b3b0b
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-19 20:23:08 +0530

    Create MicroServiceProject02OrderServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-19-08-26-WEB CLIENT AND FEIGN CLIENT/WEB CLIENT Project-2/MicroService_Project-02-OrderService/com.sub.nit.order/MicroServiceProject02OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject02OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject02OrderServiceApplication.class, args);
+	}
+
+}
