b8607ffdf5c88661e5eaccee8e543154d4c1ff7d
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-19 20:20:50 +0530

    Create MicroServiceProject02ProductServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-19-08-26-WEB CLIENT AND FEIGN CLIENT/WEB CLIENT Project-2/MicroService_Project-02-ProductService/com.sub.nit.product/MicroServiceProject02ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject02ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject02ProductServiceApplication.class, args);
+	}
+
+}
