703fe80b42464e8e672bd6fc1bcb5750ce574ff2
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-20 10:46:23 +0530

    Create MicroServiceProject03CourseServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-19-08-26-WEB CLIENT AND FEIGN CLIENT/Feign Client Project-3/MicroService_Project-03-CourseService/com.sub.nit.course/MicroServiceProject03CourseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject03CourseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject03CourseServiceApplication.class, args);
+	}
+
+}
