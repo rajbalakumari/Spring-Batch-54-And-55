fd44daf35f4dc9fecedc49c0f2e3cbb5521b7aa9
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-21 09:48:14 +0530

    Create Student.java

---
+++ ALL MICRO SERVICE CLASS/DATE-19-08-26-WEB CLIENT AND FEIGN CLIENT/Feign Client Project-3/MicroService_Project-03-StudentService/com.sub.nit.student.entity/Student.java
@@ -0,0 +1,45 @@
+package com.sub.nit.student.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer studentId;
+
+    private String studentName;
+
+    private Integer courseId;
+
+    public Student() {
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+}
