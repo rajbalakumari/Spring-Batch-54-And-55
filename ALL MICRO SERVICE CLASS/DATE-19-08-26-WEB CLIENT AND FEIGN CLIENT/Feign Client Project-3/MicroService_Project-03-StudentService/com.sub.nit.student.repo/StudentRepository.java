43264da119b121f0b152b7da47311c81d73f58b2
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-21 09:48:40 +0530

    Create StudentRepository.java

---
+++ ALL MICRO SERVICE CLASS/DATE-19-08-26-WEB CLIENT AND FEIGN CLIENT/Feign Client Project-3/MicroService_Project-03-StudentService/com.sub.nit.student.repo/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.sub.nit.student.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sub.nit.student.entity.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
