02ce79c1d47972a92e0fe0b06be4989e7de755f7
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-21 09:45:56 +0530

    Create Course.java

---
+++ ALL MICRO SERVICE CLASS/DATE-19-08-26-WEB CLIENT AND FEIGN CLIENT/Feign Client Project-3/MicroService_Project-03-StudentService/com.sub.nit.student.dto/Course.java
@@ -0,0 +1,33 @@
+package com.sub.nit.student.dto;
+
+public class Course {
+	private Integer cid;
+
+	private String cname;
+
+	private double cfees;
+
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public double getCfees() {
+		return cfees;
+	}
+
+	public void setCfees(double cfees) {
+		this.cfees = cfees;
+	}
+}
