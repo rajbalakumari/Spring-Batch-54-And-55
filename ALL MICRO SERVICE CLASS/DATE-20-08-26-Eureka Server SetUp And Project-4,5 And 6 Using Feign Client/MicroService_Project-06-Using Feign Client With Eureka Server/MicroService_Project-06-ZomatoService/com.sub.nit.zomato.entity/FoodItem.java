6c7c2436253ff27677fcbf458e35d3af83003f65
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-21 10:04:20 +0530

    Create FoodItem.java

---
+++ ALL MICRO SERVICE CLASS/DATE-20-08-26-Eureka Server SetUp And Project-4,5 And 6 Using Feign Client/MicroService_Project-06-Using Feign Client With Eureka Server/MicroService_Project-06-ZomatoService/com.sub.nit.zomato.entity/FoodItem.java
@@ -0,0 +1,48 @@
+package com.sub.nit.zomato.entity;
+
+public class FoodItem {
+	private int food_id;
+	private String food_name;
+	private double food_price;
+
+	public FoodItem() {
+
+	}
+
+	public FoodItem(int food_id, String food_name, double food_price) {
+		super();
+		this.food_id = food_id;
+		this.food_name = food_name;
+		this.food_price = food_price;
+	}
+
+	public int getFood_id() {
+		return food_id;
+	}
+
+	public void setFood_id(int food_id) {
+		this.food_id = food_id;
+	}
+
+	public String getFood_name() {
+		return food_name;
+	}
+
+	public void setFood_name(String food_name) {
+		this.food_name = food_name;
+	}
+
+	public double getFood_price() {
+		return food_price;
+	}
+
+	public void setFood_price(double food_price) {
+		this.food_price = food_price;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodItem [food_id=" + food_id + ", food_name=" + food_name + ", food_price=" + food_price + "]";
+	}
+
+}
