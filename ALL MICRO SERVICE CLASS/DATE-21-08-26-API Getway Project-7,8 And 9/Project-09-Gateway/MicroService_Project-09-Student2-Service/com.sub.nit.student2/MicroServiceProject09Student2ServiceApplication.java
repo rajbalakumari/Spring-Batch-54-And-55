1a0bab85149381c63e6497907217db423607e986
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-25 19:46:57 +0530

    Create MicroServiceProject09Student2ServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-21-08-26-API Getway Project-7,8 And 9/Project-09-Gateway/MicroService_Project-09-Student2-Service/com.sub.nit.student2/MicroServiceProject09Student2ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit.student2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject09Student2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject09Student2ServiceApplication.class, args);
+	}
+
+}
