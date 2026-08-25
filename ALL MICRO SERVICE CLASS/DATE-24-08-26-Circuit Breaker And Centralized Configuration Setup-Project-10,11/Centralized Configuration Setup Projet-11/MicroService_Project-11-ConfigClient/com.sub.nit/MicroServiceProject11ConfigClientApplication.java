327d0ab924f36fccf222e32561e5997298f96e26
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-25 20:11:09 +0530

    Create MicroServiceProject11ConfigClientApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-24-08-26-Circuit Breaker And Centralized Configuration Setup-Project-10,11/Centralized Configuration Setup Projet-11/MicroService_Project-11-ConfigClient/com.sub.nit/MicroServiceProject11ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject11ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject11ConfigClientApplication.class, args);
+	}
+
+}
