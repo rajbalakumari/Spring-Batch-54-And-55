4c16a6b8cd538f50832d8ec7a955e13a13f1790a
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-25 20:14:40 +0530

    Create MicroServiceProject10CircuitBreakerProductServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-24-08-26-Circuit Breaker And Centralized Configuration Setup-Project-10,11/Circuit Breaker Project-10/MicroService_Project-10-Circuit-Breaker-ProductService/com.sub.nit/MicroServiceProject10CircuitBreakerProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject10CircuitBreakerProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject10CircuitBreakerProductServiceApplication.class, args);
+	}
+
+}
