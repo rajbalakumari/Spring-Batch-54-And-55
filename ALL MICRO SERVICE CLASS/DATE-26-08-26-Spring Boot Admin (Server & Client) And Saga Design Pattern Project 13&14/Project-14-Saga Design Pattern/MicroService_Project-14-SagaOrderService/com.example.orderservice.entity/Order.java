70a8b5247400ccddf5f190f5cf3f07d8fb5ed8fb
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-27 21:00:31 +0530

    Create Order.java

---
+++ ALL MICRO SERVICE CLASS/DATE-26-08-26-Spring Boot Admin (Server & Client) And Saga Design Pattern Project 13&14/Project-14-Saga Design Pattern/MicroService_Project-14-SagaOrderService/com.example.orderservice.entity/Order.java
@@ -0,0 +1,64 @@
+package com.example.orderservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String productName;
+
+    private double amount;
+
+    private String status;
+
+    public Order() {
+    }
+
+    public Order(Integer id, String productName, double amount, String status) {
+        this.id = id;
+        this.productName = productName;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
