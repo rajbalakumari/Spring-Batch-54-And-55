e057bf6c0e3445fa69dafec027de3ee512102fdf
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-27 20:59:34 +0530

    Create MicroServiceProject14SagaOrderServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-26-08-26-Spring Boot Admin (Server & Client) And Saga Design Pattern Project 13&14/Project-14-Saga Design Pattern/MicroService_Project-14-SagaOrderService/com.example.orderservice/MicroServiceProject14SagaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject14SagaOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject14SagaOrderServiceApplication.class, args);
+	}
+
+}
