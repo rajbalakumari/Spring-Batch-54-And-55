34b93be9cf0ab539cbca3098a12d3e3d849de593
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-27 10:12:28 +0530

    Create ProductController.java

---
+++ ALL MICRO SERVICE CLASS/DATE-26-08-26-Spring Boot Admin (Server & Client) And Saga Design Pattern Project 13&14/Project-13-Spring Boot Admin (Server & Client)/MicroService_Project-13-Product4Service/com.sub.nit.controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.sub.nit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	@GetMapping("/product")
+	public String getProduct() {
+		return "Product Service Is Running";
+	}
+}
