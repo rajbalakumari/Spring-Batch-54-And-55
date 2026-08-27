6ba58dc3e66411b88821a57e6e2e72938e6303db
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-27 10:12:59 +0530

    Create MicroServiceProject13Product4ServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-26-08-26-Spring Boot Admin (Server & Client) And Saga Design Pattern Project 13&14/Project-13-Spring Boot Admin (Server & Client)/MicroService_Project-13-Product4Service/com.sub.nit/MicroServiceProject13Product4ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject13Product4ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject13Product4ServiceApplication.class, args);
+	}
+
+}
