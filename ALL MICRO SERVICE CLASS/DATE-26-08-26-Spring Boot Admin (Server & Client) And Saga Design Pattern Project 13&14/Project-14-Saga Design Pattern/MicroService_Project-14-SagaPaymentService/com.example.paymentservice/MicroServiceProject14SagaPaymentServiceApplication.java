8ddc96ed5bdf5b50299616b68b940fd65f0eb2f7
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-08-27 21:05:48 +0530

    Create MicroServiceProject14SagaPaymentServiceApplication.java

---
+++ ALL MICRO SERVICE CLASS/DATE-26-08-26-Spring Boot Admin (Server & Client) And Saga Design Pattern Project 13&14/Project-14-Saga Design Pattern/MicroService_Project-14-SagaPaymentService/com.example.paymentservice/MicroServiceProject14SagaPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject14SagaPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject14SagaPaymentServiceApplication.class, args);
+	}
+
+}
